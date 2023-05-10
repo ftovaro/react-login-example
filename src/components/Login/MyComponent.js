24237fcfa3b8497bfcacd5187929cf648a8be3d0
Felipe Tovar <f.tovar12@gmail.com>
2023-05-10 17:57:01 -0500

    Init commit

---
+++ src/components/Login/MyComponent.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+let myTimer;
+
+const MyComponent = (props) => {
+  const [timerIsActive, setTimerIsActive] = useState(false);
+
+  const { timerDuration } = props; // using destructuring to pull out specific props values
+
+  useEffect(() => {
+    console.log("Inside useEffect", timerIsActive);
+    if (!timerIsActive) {
+      setTimerIsActive(true);
+      myTimer = setTimeout(() => {
+        setTimerIsActive(false);
+      }, timerDuration);
+    }
+  }, [timerIsActive, timerDuration]);
+};
+
+export default MyComponent;
